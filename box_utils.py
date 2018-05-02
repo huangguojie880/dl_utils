6063a57e3284f0ad1fe8335f97154cd57abeed8a
huangguojie880 <1583649520@qq.com>
2018-05-02 20:39:34 +0800

    Update box_utils.py

--- box_utils.py
+++ box_utils.py
@@ -2,10 +2,10 @@ import xml.etree.ElementTree as ET
 
 def check_box(box_coordinates):
     '''
-    Check if the input box coordinates are legal.
+    Check if the input box coordinates are legal
     :param Rectangular_coordinates:(x1,y1,x2,y2) where x1,y1 are the coordinates of the top-left point,
-    and x2,y2 are the coordinates of the bottom-right point.
-    :return: False is illegal,True is legal.
+    and x2,y2 are the coordinates of the bottom-right point
+    :return: False is illegal,True is legal
     '''
     x1 = box_coordinates[0]
     y1 = box_coordinates[1]
@@ -18,17 +18,16 @@ def check_box(box_coordinates):
 
 def union(au, bu, area_intersection):
     '''
-    Find the area of two rectangular phases.
-    :param au:(x1,y1,x2,y2):(x1,y1)is top_left_point,(x2,y2)is right_lower_point.
-    :param bu:same as au.
+    Find the area of two rectangular phases
+    :param au:(x1,y1,x2,y2):(x1,y1)is top_left_point,(x2,y2)is right_lower_point
+    :param bu:same as au
     :param area_intersection:the area where two boxes intersect.
-    :return:area_union:area of two rectangular phases.
+    :return:area_union:area of two rectangular phases
     '''
     au_check = check_box(au)
     bu_check = check_box(bu)
     if au_check == False or bu_check == False:
-        raise ('Error:The coordinates of the upper left point are '
-               'not less than the coordinates of the lower right point')
+        raise ('Error:The coordinates of the upper left point are not less than the coordinates of the lower right point')
     area_a = (au[2] - au[0]) * (au[3] - au[1])
     area_b = (bu[2] - bu[0]) * (bu[3] - bu[1])
     area_union = area_a + area_b - area_intersection
@@ -39,15 +38,14 @@ def intersection(ai, bi):
     '''
     Find the area where two boxes intersect.
     Horizontal x axis and vertical y axis.
-    :param ai:(x1,y1,x2,y2):(x1,y1)is top_left_point,(x2,y2)is right_lower_point.
-    :param bi:Same as ai.
+    :param ai:(x1,y1,x2,y2):(x1,y1)is top_left_point,(x2,y2)is right_lower_point
+    :param bi:Same as ai
     :return:area:the area where two boxes intersect.
     '''
     ai_check = check_box(ai)
     bi_check = check_box(bi)
     if ai_check == False or bi_check == False:
-        raise ('Error:The coordinates of the upper left point are '
-               'not less than the coordinates of the lower right point')
+        raise ('Error:The coordinates of the upper left point are not less than the coordinates of the lower right point')
     x = max(ai[0], bi[0])
     y = max(ai[1], bi[1])
     w = min(ai[2], bi[2]) - x
@@ -74,7 +72,7 @@ def all_boxes(annot_path):
     '''
     The xml format is the same as pascal_voc.
     Get all boxes under the xml file.
-    :param annot_path: Xml path.
+    :param annot_path: Xml path
     :return boxes:all boxes under the xml file.
     '''
     et = ET.parse(annot_path)
@@ -89,19 +87,18 @@ def all_boxes(annot_path):
         y2 = int(round(float(obj_bbox.find('ymax').text)))
         flag = check_box([x1,y1,x2,y2])
         if flag == False:
-            raise ('Error:The coordinates of the upper left point are '
-                   'not less than the coordinates of the lower right point')
+            raise ('Error:The coordinates of the upper left point are not less than the coordinates of the lower right point')
         boxes.append([x1,y1,x2,y2])
     return boxes
 
 def max_iou(box,all_boxes):
     '''
     The maximum ratio of a given box to target boxes.
-    The box format is:(x1,y1,x2,y2).
-    :param box:given box.
-    :param all_boxes:target boxes.
-    :return:iou_num_max:maximum ratio.
-              iou_num_max_box:Corresponding box.
+    The box format is:(x1,y1,x2,y2)
+    :param box:given box
+    :param all_boxes:target boxes
+    :return:iou_num_max:maximum ratio
+              iou_num_max_box:Corresponding box
     '''
     iou_num_max = 0
     iou_num_max_box = [0, 0, 0, 0]
