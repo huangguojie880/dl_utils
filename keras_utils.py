a13b9def76935b973f15cae80d7e2c487ad120c3
huangguojie880 <1583649520@qq.com>
2018-05-05 15:52:06 +0800

    Create keras_utils.py

---
+++ keras_utils.py
@@ -0,0 +1,7 @@
+'''
+Introduce:Show training status
+Examole:
+from keras.utils import generic_utils
+progbar = generic_utils.Progbar(epoch_num)
+progbar.update(step_num,[('name1', value1)), ('name2', value2)])
+'''
