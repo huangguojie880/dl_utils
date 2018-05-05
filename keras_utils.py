94660dbbb30d4da03c8512d5dc5b994b37f964a4
huangguojie880 <1583649520@qq.com>
2018-05-05 19:16:43 +0800

    Update keras_utils.py

--- keras_utils.py
+++ keras_utils.py
@@ -1,3 +1,6 @@
+import keras.backend as K
+import tensorflow as tf
+
 '''
 Introduce:Show training status
 Examole:
@@ -5,3 +8,12 @@ from keras.utils import generic_utils
 progbar = generic_utils.Progbar(epoch_num)
 progbar.update(step_num,[('name1', value1)), ('name2', value2)])
 '''
+
+def focal_mse(r = 1):
+    '''
+    A focal mse loss function
+    :param r: The coefficient is several times
+    :return: -
+    '''
+    def mse(y_true, y_pred):
+        return K.mean((tf.abs(y_true - y_pred)**r)*tf.losses.sigmoid_cross_entropy( y_pred,y_true))
