147f652eb40f11ba1c0436b1fc738a2577a974b2
huangguojie880 <1583649520@qq.com>
2018-05-05 15:37:54 +0800

    Update random_utils.py

--- random_utils.py
+++ random_utils.py
@@ -1,1 +1,10 @@
+import random
 
+
+'''
+Introduce:Randomly extract n elements from a list
+Tips:n is less than the length of the list
+Example:
+l = [2 ,567,89 , 9 , 44,77]
+l_sample = random.sample(l,3)
+'''
